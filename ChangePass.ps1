3b614412ce638d32ac8d3be0537d1bacd986e1ab
zzzuser999 <87768416+zzzuser999@users.noreply.github.com>
2023-04-05 12:51:20 +0200

    Add files via upload

---
+++ ChangePass.ps1
@@ -0,0 +1,3 @@
+ Set-ADAccountPassword -Identity 'CH-HFM_IGSOLVER' -Server ABB.com `
+-OldPassword (ConvertTo-SecureString -AsPlainText 'SN8oJuh#rAfSdDAS' -Force) `
+-NewPassword (ConvertTo-SecureString -AsPlainText '1LZCsYeJkr80IcNSga5r' -Force)
